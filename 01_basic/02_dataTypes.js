da33e16203fc4fc19625dc566f081c4a1d79bf49
Milan Lamsal <milan123lamsal@gmail.com>
2025-02-10 01:46:01 +0000

    Datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all JS code as newer version
+// alert(3+3); using nodejs, not browser.
+console.log(3+3); 
+console.log("Debal");
+//for the documentation go to https://tc39.es/ecma262/ (tc39) & mdn
+
+let name= "Debal";//string
+let age= 19; // number
+let isLoggedIn= false // boolean 
+
+/**
+ * primitive data types
+ * 
+ * num
+ * bigint
+ * string= ""
+ * boolean =true/false
+ * null= standalone value
+ * undefined=> value not assigned , int state;
+ * symbol=> unique
+ * 
+ * object 
+*/
+console.log(typeof "Debal");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

dce31d8f2591574f8c4032060e6a5903accc5d51
Milan Lamsal <milan123lamsal@gmail.com>
2025-02-11 14:38:47 +0000

    Special Type of Conversion

---
+++ 01_basic/04_stringToNumber.js
@@ -0,0 +1,57 @@
+let value = 3;
+let negativeValue = -value;
+console.log(negativeValue);
+
+// Operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+console.log(2 ** 3); // power2^2
+// console.log(2/2);
+//console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1 + str2;// concatination
+console.log(str3);
+
+/*What if*/
+/*  console.log("1"+2);//=> 12
+ console.log(1+"2");//=>12
+  console.log("1"+2+2);//=>122
+ console.log(1+"2"+2);//=>122
+ console.log(1+2+"2");//=>32
+ So if the string comes first then all will be string and if the number comes first then it will 
+ add and then strin*/
+
+//Special type of Converstion //
+console.log(true);
+console.log(+true); //=> 1 (increment in true )
+//console.log(true+);
+console.log(+"");//=> 0
+
+let num1, num2, num3 ;
+num1=num2=num3= 2 + 2;
+
+let gameCounter=100;
+// gameCounter++; =>postfix operator 
+++gameCounter; // prefix operator 
+console.log(gameCounter);
+
+/**
+ * links to study->https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment // prefix and postfix js in mdn
+ * 
+ * https://tc39.es/ecma262/multipage/abstract-operations.html // type conversion documentation in tc39
+ */
+
+
+
+
+
+
+
+
+
+
+
+

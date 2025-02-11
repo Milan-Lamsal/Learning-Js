d098dac19934c8af570c9d86872b62600ab48c5f
Milan Lamsal <milan123lamsal@gmail.com>
2025-02-11 10:02:49 -0500

    Update 04_stringToNumber.js
    Added Notes

--- 01_basic/04_stringToNumber.js
+++ 01_basic/04_stringToNumber.js
@@ -39,9 +39,29 @@ let gameCounter=100;
 console.log(gameCounter);
 
 /**
- * links to study->https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment // prefix and postfix js in mdn
- * 
- * https://tc39.es/ecma262/multipage/abstract-operations.html // type conversion documentation in tc39
+ * links to study->https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment =>prefix and postfix js in mdn
+ 
+ * ✔ Prefix (++x, --x): Increments/Decrements first, then returns the new value.
+  let x = 5;
+  let y = ++x; // x is increased first, then assigned to y.
+  
+  console.log(x); // 6
+  console.log(y); // 6
+  
+   ✔ Postfix (x++, x--): Returns the original value first, then increments/decrements.
+  let a = 5;
+  let b = a++; // a is assigned to b first, then incremented.
+
+console.log(a); // 6 (a is increased after assignment)
+console.log(b); // 5 (b gets the original value of a)
+
+  
+ 
+ * https://tc39.es/ecma262/multipage/abstract-operations.html // type conversion documentation in tc39 
+✅ + prefers concatenation if a string is involved.
+✅ -, *, /, % prefer numeric conversion.
+✅ Operator precedence applies (*, /, % before +).
+✅ Use Number(), String(), Boolean() for explicit conversion.
  */
 
 

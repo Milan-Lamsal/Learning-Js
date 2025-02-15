79dbec41db2aa87a7e2b7c0a3e9cc9e4b1f3ec1f
Milan Lamsal <milan123lamsal@gmail.com>
2025-02-15 00:35:34 +0000

    DOM_SCRIPT

---
+++ Question_Practices/DOM_script.js
@@ -0,0 +1,38 @@
+// console.log("Hello");
+// // window.alert("YO man");
+
+// // console.dir(document.body);
+// console.log(document.log);
+// document.body.style.background="green"; // DOM
+ 
+// let heading =document.getElementById("Heading");
+// console.dir(heading);
+
+// let heading =document.getElementsByClassName("heading-class");
+// console.log(heading);
+// console.dir(heading);
+
+// let para=document.getElementsByTagName("p");
+// console.log(para);
+// console.dir(para);
+
+let firElem= document.querySelector("p");
+console.dir(firElem);
+
+
+let allEl= document.querySelectorAll("p");
+console.dir(allEl);
+
+// let div= document.querySelector("div").children; // in console.
+
+let div= document.querySelector("div");
+console.dir(div);
+
+let heading = document.querySelector("h1");
+console.dir(heading);
+
+
+
+
+
+

e60d638fd0066fdaaa4afb9fba818e58675e0ab8
Milan Lamsal <milan123lamsal@gmail.com>
2025-02-21 19:10:55 +0000

    ErrorHanldingJS

---
+++ Question_Practices/ErrorHandling.js
@@ -0,0 +1,32 @@
+// TRY CATCH BLOCK  
+let a =7;
+let b=10;
+console.log("a=" ,a);
+console.log("b=" ,b);
+console.log("a+b=" ,a+b);
+
+try{
+    console.log("a-b=" ,a-c);// error because of which rest of the code will not be executed
+}
+catch(error){
+    console.log("Error: ",error);
+}
+//  using try and catch block we can handle the error and rest of the code will be executed.
+
+
+console.log("a*b=" ,a*b);
+
+console.log("a/b=" ,a/b);
+console.log("a%b=" ,a%b);
+console.log("a**b=" ,a**b);
+console.log("a++=" ,a++);
+console.log("a--=" ,a--);
+console.log("++a=" ,++a);
+console.log("--a=" ,--a);
+console.log("a+=b" ,a+=b);
+console.log("a-=b" ,a-=b);
+console.log("a*=b" ,a*=b);
+console.log("a/=b" ,a/=b);
+console.log("a%=b" ,a%=b);
+console.log("a**=b" ,a**=b);
+console.log("a==b" ,a==b);

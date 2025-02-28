e52206ad2c3a85d43ff95281c5b3b1fb04091788
Milan Lamsal <milan123lamsal@gmail.com>
2025-02-28 03:12:12 +0000

    Switch_Case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+//  switch case 
+// switch(key){
+//     case value:
+//     break;
+
+//     default:
+//     break;
+// }
+
+const month= 2// "March"
+switch (month) {
+    case 1:// "Jan"
+        console.log("January");
+        break;
+    case 2 :// "Feb":
+        console.log("Febrauray");
+        break;
+        
+    case 3://March:
+        console.log("March");
+        break 
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case Match");
+        break;
+}
+// Switch case , break 

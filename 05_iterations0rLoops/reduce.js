dd4758e8cff4a0d0e17bf901192f603d71f7d040
Milan Lamsal <milan123lamsal@gmail.com>
2025-03-01 17:42:11 +0000

    Added different ways to do

--- 05_iterations0rLoops/reduce.js
+++ 05_iterations0rLoops/reduce.js
@@ -15,9 +15,43 @@ console.log(sumWithInitial);
 // Expected output: 10 */
 
 const myNums =[1,2,3]
-let myTotal = myNums.reduce(function ( acc, currVal){
-  console.log(`acc:${acc} and currVal:${currVal)`);
-    return acc+currVal;
+// let myTotal = myNums.reduce(function ( acc, currVal){
+//   console.log(`acc:${acc} and currVal: ${currVal}`);
+//     return acc+currVal;
 
-}, 0)// giving 0 to acc 
+// }, 0) // acc :0
+
+// console.log(myTotal);
+
+/**
+acc:0 and currVal: 1
+acc:1 and currVal: 2
+acc:3 and currVal: 3
+ */
+
+//  wE can do in another way as well 
+
+const myTotal =myNums.reduce((acc, currVal)=> acc+ currVal, 0);
 console.log(myTotal);
+
+const shoppingCart=[
+  {
+    itemName: 'js course',
+    price: 499
+  },
+  {
+    itemName: 'python course',
+    price:599
+  },
+  {
+    itemName: 'App dev',
+    price:1499
+  },
+  {
+    itemName: 'DSA course',
+    price:799
+  }
+]
+let finalPrice= shoppingCart.reduce((acc, currVal)=> acc + currVal.price , 0 ) // here currVal represent 499
+
+console.log(finalPrice);
